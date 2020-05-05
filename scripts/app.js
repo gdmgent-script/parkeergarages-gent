e4dd1dea600ff68469e5a997a35cdf3202a30963
Frederick Roegiers <frederick@rogerthat.be>
2020-05-05 11:35:53 +0200

    wip

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,31 @@
 // declaratie van de overview
 const parkingOverview = document.getElementById('parking-overview');
 
+
+fetch('REPLACE-THIS-WITH-CORRECT-JSON-DATA-UR')
+  .then(function(response) {
+    // retourneer de response, vertaald naar een door JS-intepreteerbaar formaat
+  })
+  .then(function(data) {
+    console.log(data);
+    // doe iets met die data (netjes: via een andere functie)
+    showParkingGent(data);
+  })
+  .catch(function(err) {
+    console.log('Ai ai ai, an error', err);
+  });
+
+
+  function showParkingGent(parkings){
+        // itereer door alle parkings
+        for(const parking of parking) {  
+          // haal de noodzakelijke properties uit het parking-object
+
+          // plaats ze in de dom (zie voorbeeld in html)
+
+      }
+
+  }
+
+
+
